b4dcfb421a4b8cfd5443c13c55300a29ff2ba99e
Jose Molina <josernr25@gmail.com>
2020-12-18 10:45:16 -0300

    Add prueba SignalR

---
+++ Hubs/PositionHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaSignalR.Hubs
+{
+    public class PositionHub : Hub
+    {
+        public async Task SendPosition(int left, int top)
+        {
+            await Clients.Others.SendAsync("ReceivePosition", left, top);
+        }
+    }
+}
